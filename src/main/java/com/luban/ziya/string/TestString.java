2760d817eab145bde366a6922016a618266783e9
ziya <644735414@qq.com>
2020-08-04 23:11:00 -0700

    类加载测试代码

--- src/main/java/com/luban/ziya/string/TestString.java
+++ src/main/java/com/luban/ziya/string/TestString.java
@@ -38,7 +38,7 @@ public class TestString {
     }
 
     public static void test2() {
-        String s1 = new String("11");
+        String s1 = new String("");
         String s2 = new String("11");
 
         System.out.println(s1 == s2);
