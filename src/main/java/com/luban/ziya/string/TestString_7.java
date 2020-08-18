8aff9a2e92b5dcae40650fbf728f102c4a800188
ziya <644735414@qq.com>
2020-08-17 22:25:10 -0700

    string测试代码

--- src/main/java/com/luban/ziya/string/TestString_7.java
+++ src/main/java/com/luban/ziya/string/TestString_7.java
@@ -7,10 +7,29 @@ package com.luban.ziya.string;
 public class TestString_7 {
 
     public static void main(String[] args) {
-//        String str = "子牙" + new String("子牙");
+//        String s1 = new String("子牙真帅");
+        String s2 = "子牙" + "子牙";
+        String s3 = "子牙" + new String("真帅");
 
-        String str = new String("A" + "B");
+//        String s4 = "子牙" + "真帅";
+
+
+        test1();
 
         while (true);
+    }
+
+    public static void test() {
+        String s1 = "子牙" + "子牙";
+
+        String s2 = "子牙";
+    }
+
+    public static void test1() {
+        String s = "子牙";
+
+        String s1 = s + s;
+
+        String s2 = "子牙";
     }
 }
