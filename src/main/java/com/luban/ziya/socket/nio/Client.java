ed17e7a6fb00df8f23b00e498c36c9a8ad28103a
ziya <644735414@qq.com>
2020-10-29 06:44:26 -0700

    nio案例

---
+++ src/main/java/com/luban/ziya/socket/nio/Client.java
@@ -0,0 +1,80 @@
+package com.luban.ziya.socket.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+/**
+ * Created By ziya
+ * 2020/10/29
+ */
+public class Client {
+
+    public static final String ip = "127.0.0.1";
+    public static final int port = 6688;
+    Selector selector;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        client.connect();
+        client.listen();
+    }
+
+    public void connect() {
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+
+            selector = Selector.open();
+
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+
+            socketChannel.connect(new InetSocketAddress(ip, port));
+
+            System.out.println("#1 发起连接服务器...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listen() {
+        while (true) {
+            try {
+                int event = selector.select();
+
+                if (event > 0) {
+                    Iterator<SelectionKey> selectionKeys = selector.selectedKeys().iterator();
+                    while (selectionKeys.hasNext()) {
+                        SelectionKey selectionKey = selectionKeys.next();
+                        selectionKeys.remove();
+
+                        if (selectionKey.isConnectable()) {
+                            SocketChannel channel = (SocketChannel) selectionKey.channel();
+                            if (channel.isConnectionPending()) {
+                                channel.finishConnect();
+                            }
+
+                            System.out.println("#2 已连上服务器...");
+
+                            channel.configureBlocking(false);
+
+                            channel.register(selector, SelectionKey.OP_READ);
+
+                            channel.write(ByteBuffer.wrap("Hello, Server".getBytes()));
+                        } else if (selectionKey.isReadable()) {
+
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
